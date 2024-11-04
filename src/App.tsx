a05673633bb00848f135e740b902d29bc0727db3
Mangesh Pangam <135334519+Mangesh2704@users.noreply.github.com>
2024-11-04 20:15:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { GET_TICKETS_URL } from './constants';
 import { loadGrid, mapUsersByUserId } from './utils';
 import { Ticket, User } from './interfaces';
 import Loader from './components/Loader';
-import './App.css'
+import './App.css';
 
 function App() {
   const [tickets, setTickets] = useState<Ticket[]>([]);
@@ -15,43 +15,46 @@ function App() {
   const [ordering, setOrdering] = useState<string>("priority");
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    loadSettings();
-    fetch(GET_TICKETS_URL).then(resp => resp.json()).then(res => {
-      const { tickets, users } = res;
-      setTickets(tickets);
-      setUserData(mapUsersByUserId(users));
-    }).catch(err => { });
-  }, [])
+  const loadSettings = useCallback(() => {
+    setGrouping(localStorage.getItem("grouping") || "status");
+    setOrdering(localStorage.getItem("ordering") || "priority");
+  }, []);
+
+  const saveSettings = useCallback((data: Record<string, string>) => {
+    for (let key in data) {
+      localStorage.setItem(key, data[key]);
+    }
+  }, []);
 
   useEffect(() => {
-    if (!tickets.length)
-      return;
+    loadSettings();
+    fetch(GET_TICKETS_URL)
+      .then(resp => resp.json())
+      .then(res => {
+        const { tickets, users } = res;
+        setTickets(tickets);
+        setUserData(mapUsersByUserId(users));
+      })
+      .catch(err => { });
+  }, [loadSettings]); // include loadSettings here
+
+  useEffect(() => {
+    if (!tickets.length) return;
     setGridData(loadGrid(tickets, grouping, ordering));
     setLoading(false);
-  }, [grouping, ordering, tickets])
+  }, [grouping, ordering, tickets]);
 
   const onSetGrouping = useCallback((value: string) => {
     setLoading(true);
     setGrouping(value);
     saveSettings({ grouping: value });
-  }, []);
+  }, [saveSettings]); // include saveSettings here
 
   const onSetOrdering = useCallback((value: string) => {
     setLoading(true);
     setOrdering(value);
     saveSettings({ ordering: value });
-  }, []);
-
-  const saveSettings = useCallback((data: Record<string, string>) => {
-    for (let key in data)
-      localStorage.setItem(key, data[key]);
-  }, []);
-
-  const loadSettings = useCallback(() => {
-    setGrouping(localStorage.getItem("grouping") || "status");
-    setOrdering(localStorage.getItem("ordering") || "priority");
-  }, []);
+  }, [saveSettings]); // include saveSettings here
 
   return (
     <div className="App">
